46d3430790b26851c5c5886cd032c9a5a2e6cbe7
Esteban Meza <esteban@notus.cl>
2022-02-03 20:48:32 -0300

    fix github token

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -5,7 +5,7 @@ def call()
       environment {
           NEXUS_USER         = credentials('NEXUS-USER')
           NEXUS_PASSWORD     = credentials('NEXUS-PASS')
-          GITHUB_TOKEN       = credentials('github_token')
+          GITHUB_TOKEN       = credentials('token_github')
       }
       parameters {
           choice(
