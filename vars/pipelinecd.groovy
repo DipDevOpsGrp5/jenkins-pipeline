d68d4a37f1029354f6144e86f3694dd05bb6b59c
Esteban Meza <esteban@notus.cl>
2022-02-03 21:55:56 -0300

    Add merge methods

--- vars/pipelinecd.groovy
+++ vars/pipelinecd.groovy
@@ -1,6 +1,7 @@
 def call(){
   stage('Git Diff'){
-    sh 'git diff $BRANCH_NAME main'
+    // sh 'git diff $BRANCH_NAME main'
+    sh "${POM_VERSION} test"
   }
   stage("Paso 5: Descargar Nexus"){
       sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:8081/repository/devops-laboratorio/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
@@ -12,12 +13,58 @@ def call(){
       sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
   }
   stage("Merge main"){
-
+    mergeBranch(main)
   }
   stage("Merge develop"){
-
+    mergeBranch(develop)
   }
   stage("Tag main"){
 
   }
+}
+
+
+def mergeBranch(String baseBranch) {
+    print ("merging branch" + BRANCH_NAME + ' into ' + baseBranch)
+    SHA = sh (
+        script:
+            """
+                curl -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/DipDevOpsGrp5/ms-iclab/git/refs/heads/$BRANCH_NAME | jq -r '.object.sha'
+            """,
+        returnStdout: true
+    ).trim()
+
+    print (SHA)
+
+    sh """
+        curl -X POST -H "Accept 'application/vnd.github.v3+json'" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/DipDevOpsGrp5/ms-iclab/merges -d '{"head":"$SHA","base":"$baseBranch"}'
+    """
+}
+
+def tagMainBranch() {
+    print ("tagging main branch")
+    SHA = sh (
+        script:
+            """
+                curl -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/DipDevOpsGrp5/ms-iclab/git/refs/heads/main | jq -r '.object.sha'
+            """
+        returnStdout: true
+    ).trim()
+
+    print (SHA)
+
+    SHA_TAG = sh (
+        script:
+        """
+            curl -X POST -H "Accept 'application/vnd.github.v3+json'" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/DipDevOpsGrp5/ms-iclab/git/tags -d '{"tag":"$ARTIFACT_VERSION", "message":"$ARTIFACT_VERSION", "object": "$SHA", "type": "commit"}' | jq -r '.sha'
+        """,
+        returnStdout: true
+
+    ).trim()
+
+    print('SHA_TAG: ' + SHA_TAG)
+
+    sh """
+        curl -X POST -H "Accept 'application/vnd.github.v3+json'" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/DipDevOpsGrp5/ms-iclab/git/refs -d '{"ref":"refs/tags/$ARTIFACT_VERSION", "sha": "$SHA_TAG"}'
+    """
 }
